e63f0e50c3a0624133af648197dc34f21ca0edd6
SteveCordero <stevecords2008@gmail.com>
2022-03-08 08:49:21 -0800

    Finished a working version of a queue in regards to expected output

--- src/SteveQueue.java
+++ src/SteveQueue.java
@@ -1,44 +1,97 @@
 public class SteveQueue {
     //instance variables
     private SteveNode head;
+    private SteveNode tail;
+    private static int count;
 
 
     //constructors
     public SteveQueue()
     {
         head = null;
+        //tail = head;
+        count = 0;
     }
 
     //brain methods
-    public void addSteveNode()
+    public void addToBack(int info)
     {
 
+        //head.setsLinkPrev(head);
+        if(count == 0)
+        {
+            head = new SteveNode(info , head);//first node in the queue
+            //head.getsLink().setsLink(head);
+            tail = new SteveNode(head);
+        }//the head will stay still
+        else
+        {
+            head = new SteveNode(info , head);
+            head.getsLink().setsLink(head);
+//            tail = new SteveNode(info , tail);
+//            tail.setsLink(tail.getsLink());
+        }//the tail will grow
+//        else
+//        {
+//            tail = new SteveNode(info, tail);
+//
+//        }
+
+        count++;
     }
 
-    public void removeSteveNode()
+    public void removeFromFront()
     {
+//        while(head !=null)
+//        {
+//            head.getsLink();
+//        }
+        if(head != null)
+        {
 
+            tail = tail.getsLink();
+        }
+        else
+        {
+            System.out.println("Delete from an empty list?");
+            System.exit(0);
+        }
+        count--;
     }
 
     public void printOut()
     {
-
-    }
+        SteveNode position  = tail.getsLink();
+        //System.out.println(head.getsData());
+        //System.out.println(tail.getsLink().getsData());
+       for(int i = 0; i < count; i++)
+       {
+            System.out.println(position.getsData());
+            System.out.println("-");
+            position = position.getsLink();
+        }
+    }//this method will printout the entire current queue list
 
     public void length()
     {
-
+        System.out.println("Length of the Queue is: " + count);
     }
 
     private class SteveNode{
         //instance variables
         private int sData;
         private SteveNode sLink;
+        //private SteveNode sLinkPrev;
 
         //Constructors
         public SteveNode(int sData, SteveNode sLink)
         {
             this.sData = sData;
+            this.sLink = sLink;
+        }
+
+        public SteveNode(SteveNode sLink)
+        {
             this.sLink = sLink;
         }
 
@@ -49,6 +102,27 @@ public class SteveQueue {
 
         public SteveNode getsLink() {
             return sLink;
+        }
+
+        //public SteveNode getsLinkPrev()
+        //{
+        //    return sLinkPrev;
+        //}
+
+        public void setsLink(SteveNode sLink)
+        {
+            this.sLink = sLink;
+        }
+
+        //public void setsLinkPrev(int data ,SteveNode sLinkPrev)
+        //{
+        //    this.sLinkPrev = sLinkPrev;
+        //}
+
+        //toString
+        public String toString()
+        {
+            return "data: " + sData + " links to " + sLink;
         }
     }//end inner class node
 }//end Steve Queue Class
--- src/SteveQueueDriver.java
+++ src/SteveQueueDriver.java
@@ -3,6 +3,37 @@ public class SteveQueueDriver {
     public static void main(String args[])
     {
         SteveQueue queue = new SteveQueue();
+        queue.addToBack(7);
+        queue.addToBack(8);
+        queue.addToBack(100);
+        queue.addToBack(9);
+        queue.addToBack(10);
 
+
+        queue.printOut();
+        System.out.println();
+        queue.length();
+
+        queue.removeFromFront();
+        System.out.println();
+
+        queue.printOut();
+        System.out.println();
+        queue.length();
+
+
+        queue.removeFromFront();
+        System.out.println();
+
+        queue.addToBack(70);
+        queue.printOut();
+        queue.length();
+
+        queue.removeFromFront();
+        System.out.println();
+        queue.printOut();
+        queue.length();
+
+        //queue.printOut();
     }//end main method
 }//end SteveQueueDriver
